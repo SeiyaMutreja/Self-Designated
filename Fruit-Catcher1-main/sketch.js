634df0fddbdb2ca05736829ef3eba7a441d72310
SeiyaMutreja <73780380+SeiyaMutreja@users.noreply.github.com>
2021-03-31 10:42:59 +0530

    Add files via upload

---
+++ Fruit-Catcher1-main/sketch.js
@@ -0,0 +1,48 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var rAppleImg, gAppleImg;
+var redApple;
+var greenApple;
+var player_img;
+
+
+function preload(){
+  back_img = loadImage("images/background.jpg");
+  player_img = loadImage("images/basket2.png");
+  rAppleImg = loadImage("images/Redapple.png");
+  gAppleImg = loadImage("images/greenApple.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+         
+     game.end();
+   }
+}
